9c1da14b8dea40520f10c4554fae6b3d325a98cf
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-01-09 15:23:57 +0100

    added two more sections and modified svg files for the icons

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,6 +15,7 @@
                 <h1 class="my-title">New music video</h1>
             </div>
         </section>
+        <!-- ALBUM CARDS SECTION -->
         <section class="album-section">
             <div class="text-center">
                 <p class="my-subtitle">who we are</p>
@@ -26,7 +27,37 @@
                 </article>
             </div>
         </section>
-        
+        <!-- NEW ALBUM SECTION -->
+        <section class="new-album-section container-fluid">
+            <div class="new-album-info">
+                <p class="my-subtitle">Instrumental rock</p>
+                <h1 class="my-title">Album out now</h1>
+                <button class="my-button">Read more</button>
+            </div>
+        </section>
+        <!-- STATISTICS SECTION -->
+        <section class="statistics-section">
+            <article class="statistic">
+                <img src="../assets/image (13).svg" alt="concerts icon">
+                <h1 class="m-0">163</h1>
+                <p class="m-0">concerts</p>
+            </article>
+            <article class="statistic">
+                <img src="../assets/image (14).svg" alt="concerts icon">
+                <h1 class="m-0">163</h1>
+                <p class="m-0">concerts</p>
+            </article>
+            <article class="statistic">
+                <img src="../assets/image (15).svg" alt="concerts icon">
+                <h1 class="m-0">163</h1>
+                <p class="m-0">concerts</p>
+            </article>
+            <article class="statistic">
+                <img src="../assets/image (16).svg" alt="concerts icon">
+                <h1 class="m-0">163</h1>
+                <p class="m-0">concerts</p>
+            </article>
+        </section>
     </main>
 </template>
 <script>
@@ -92,6 +123,42 @@ export default {
             h1{
                 margin-top: 2rem;
             }
+        }
+    }
+    .new-album-section{
+        background-image: url(../assets/cms_parallax.jpg);
+        background-position: center;
+        background-size: cover;
+    }
+    .new-album-info{
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        justify-content: center;
+        gap: 2rem;
+        padding: 10rem 0rem;
+        width: 80%;
+        margin: 0 auto;
+    }
+    .statistics-section{
+        display: flex;
+        justify-content: space-around;
+        padding: 6rem 7.5rem;
+        background-image: url(../assets/counter_parallax.jpg);
+        background-position: top;
+        background-size: cover;
+    }
+    .statistic{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: center;
+        justify-content: center;
+        img{
+            width: 55px;
+        }
+        p{
+            text-transform: uppercase;
         }
     }
 </style>
